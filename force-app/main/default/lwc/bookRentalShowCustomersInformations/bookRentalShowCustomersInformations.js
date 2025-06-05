f65b610a858a104df677d79d2ccc78211da6ef42
irshad-khan-1993-ibirdsservices <irshad@ibirdsservices.com>
2025-06-05 13:06:22 +0530

    files modified

--- force-app/main/default/lwc/bookRentalShowCustomersInformations/bookRentalShowCustomersInformations.js
+++ force-app/main/default/lwc/bookRentalShowCustomersInformations/bookRentalShowCustomersInformations.js
@@ -14,21 +14,21 @@ export default class BookRentalShowCustomersInformations extends LightningElemen
     @api SelectABook;
     @api EnterFirstNameAndLastName;
     @api isCustomerAlreadyExists;
-    get isCustomerAlreadyExists2() {return this.isCustomerAlreadyExists;}
     // @track MailingStreet;
     // @track MailingPostalCode;
     // @track MailingCity;
     // @track MailingState;
     // @track MailingCountry;
     // @track searchAddressEntries;
-
+    
+    get isCustomerAlreadyExists2() {return this.isCustomerAlreadyExists;}
     get MailingStreet() {return this.EnterFirstNameAndLastName["SearchAddress-Block"].MailingStreet;}
     get MailingPostalCode() {return this.EnterFirstNameAndLastName["SearchAddress-Block"].MailingPostalCode;}
     get MailingCity() {return this.EnterFirstNameAndLastName["SearchAddress-Block"].MailingCity;}
     get MailingState() {return this.EnterFirstNameAndLastName["SearchAddress-Block"].MailingState;}
     get MailingCountry() {return this.EnterFirstNameAndLastName["SearchAddress-Block"].MailingCountry;}
     connectedCallback(){
-        console.log('connectedCallback called---');        
+        console.log('BookRentalShowCustomersInformations:connectedCallback called');        
     }
     
 }
